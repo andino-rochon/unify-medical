930282ca69d2f4f203ab976d9cc7c61d64371e69
Kruthika484 <57548645+Kruthika484@users.noreply.github.com>
2021-02-06 17:05:27 -0500

    Update RegistrationPage.java

--- src/main/java/giacom/RegistrationPage.java
+++ src/main/java/giacom/RegistrationPage.java
@@ -29,6 +29,7 @@ class RegistrationPage extends JFrame implements ActionListener {
     private JLabel gender;
     private JRadioButton male;
     private JRadioButton female;
+    private JRadioButton otherGen;
     private ButtonGroup gengp;
     private JLabel dob;
     private JComboBox date;
@@ -75,8 +76,7 @@ class RegistrationPage extends JFrame implements ActionListener {
 
     // constructor, to initialize the components
     // with default values.
-    public RegistrationPage()
-    {
+    public RegistrationPage() {
         setTitle("Registration Form");
         setBounds(300, 90, 900, 1000);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -129,6 +129,7 @@ class RegistrationPage extends JFrame implements ActionListener {
         tAddressL1.setLocation(100, 160);
         c.add(tAddressL1);
 
+        streetName = tAddressL1.getText();
 
         addressL2 = new JLabel("Address Line 2");
         addressL2.setFont(new Font("Arial", Font.PLAIN, 20));
@@ -180,6 +181,7 @@ class RegistrationPage extends JFrame implements ActionListener {
         tzipC.setLocation(100, 400);
         c.add(tzipC);
 
+        citStatZip = tcity.getText() + "," + tstate.getText() + " " + tzipC.getText();
 
         gender = new JLabel("Gender");
         gender.setFont(new Font("Arial", Font.PLAIN, 20));
@@ -201,9 +203,25 @@ class RegistrationPage extends JFrame implements ActionListener {
         female.setLocation(275, 440);
         c.add(female);
 
+        otherGen = new JRadioButton("Other");
+        otherGen.setFont(new Font("Arial", Font.PLAIN, 15));
+        otherGen.setSelected(true);
+        otherGen.setSize(75, 20);
+        otherGen.setLocation(350, 440);
+        c.add(otherGen);
+
         gengp = new ButtonGroup();
         gengp.add(male);
         gengp.add(female);
+        gengp.add(otherGen);
+
+        if (male.isSelected()) {
+            gend = "Male";
+        } else if (female.isSelected()) {
+            gend = "Female";
+        } else {
+            gend = "Other";
+        }
 
         dob = new JLabel("Date of Birth");
         dob.setFont(new Font("Arial", Font.PLAIN, 18));
@@ -229,6 +247,8 @@ class RegistrationPage extends JFrame implements ActionListener {
         year.setLocation(360, 480);
         c.add(year);
 
+        datBirth = month.toString() + " " + date.toString() + " " + year.toString();
+
         //username is the email address
         username = new JLabel("Username (Enter your email address):");
         username.setFont(new Font("Arial", Font.PLAIN, 20));
@@ -242,6 +262,7 @@ class RegistrationPage extends JFrame implements ActionListener {
         tusername.setLocation(450, 520);
         c.add(tusername);
 
+        usrName = tusername.getText();
 
         password = new JLabel("Password:");
         password.setFont(new Font("Arial", Font.PLAIN, 20));
@@ -255,6 +276,7 @@ class RegistrationPage extends JFrame implements ActionListener {
         tpassword.setLocation(200, 560);
         c.add(tpassword);
 
+        pass = tpassword.getText();
 
         term = new JCheckBox("Accept Terms And Conditions.");
         term.setFont(new Font("Arial", Font.PLAIN, 15));
@@ -318,7 +340,7 @@ class RegistrationPage extends JFrame implements ActionListener {
             date.setSelectedIndex(0);
             month.setSelectedIndex(0);
             year.setSelectedIndex(0);
-            
+
         }
     }
 }
